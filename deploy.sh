3c26ed9172f5ef2486502fb5e3f71fcb3e9b2922
Alexis Benoist <alexis.benoist@gmail.com>
2015-05-22 19:55:04 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@ rm -r build
 rm -r dist
 pandoc --from=markdown --to=rst README.md --output=readme.rst
 python setup.py bdist_wheel --universal
+python setup.py sdist
 twine upload dist/*
 open https://pypi.python.org/pypi/ERAlchemy
