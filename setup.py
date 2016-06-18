aef0b3a59a98fb48afea2c094da82ab2ddae2c9c
Alexis Benoist <alexis.benoist@gmail.com>
2016-06-18 16:22:26 +0200

    setup.py: added python3.5 support

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Scientific/Engineering :: Visualization',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Database',
