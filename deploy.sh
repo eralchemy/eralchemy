73597593f3db47891b69e8670098f39fdf7a97ef
Alexis Benoist <alexis.benoist@gmail.com>
2016-06-24 00:20:53 +0200

    Removed deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-rm -r ERAlchemy.egg-info
-rm -r build
-rm -r dist
-pandoc --from=markdown --to=rst README.md --output=readme.rst
-python setup.py bdist_wheel --universal
-python setup.py sdist
-twine upload dist/*
-open https://pypi.python.org/pypi/ERAlchemy
