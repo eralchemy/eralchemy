04753364017e1e3657b3bc3ace62fa847e7480e1
Alexis Benoist <alexis.benoist@gmail.com>
2018-02-25 10:52:27 +0100

    fix: python 3

--- eralchemy/helpers.py
+++ eralchemy/helpers.py
@@ -12,7 +12,7 @@ def check_args(args):
     """Checks that the args are coherent."""
     check_args_has_attributes(args)
     if args.v:
-        non_version_attrs = [v for k, v in args.__dict__.iteritems()if k != 'v']
+        non_version_attrs = [v for k, v in args.__dict__.items()if k != 'v']
         print('non_version_attrs', non_version_attrs)
         if len([v for v in non_version_attrs if v is not None]) != 0:
             fail('Cannot show the version number with another command.')
