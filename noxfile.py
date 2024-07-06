9c05eea5c592c796891f605f22efbf6b9f47825f
Rambaud Pierrick <12rambau@users.noreply.github.com>
2024-07-06 18:05:10 +0000

    updatre gitignore and add a lint session

---
+++ noxfile.py
@@ -0,0 +1,15 @@
+"""All the process that can be run using nox.
+
+The nox run are build in isolated environment that will be stored in .nox. to force the venv update, remove the .nox/xxx folder.
+"""
+
+import nox
+
+nox.options.sessions = ["lint"]
+
+
+@nox.session(reuse_venv=True)
+def lint(session):
+    """Apply the pre-commits."""
+    session.install("pre-commit")
+    session.run("pre-commit", "run", "--all-files", *session.posargs)
