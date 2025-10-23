4fb8a0e00e9137f71a4acf624a889fa0fb322fb2
Yoshiharu Yamada <yoshiharuseijin@gmail.com>
2025-10-23 14:31:06 +0000

    Add tests for mermaid

--- tests/test_mermaid.py
+++ tests/test_mermaid.py
@@ -1,4 +1,4 @@
-from eralchemy.models import sanitize_mermaid
+from eralchemy.models import Column, sanitize_mermaid
 
 
 def test_mermaid_escape():
@@ -11,3 +11,15 @@ def test_mermaid_escape():
     assert "test7" == sanitize_mermaid("test7", is_er=True)
     assert "public_table_column" == sanitize_mermaid("public.table.column")
     assert "public_table_column" == sanitize_mermaid("public%table!column")
+
+
+def test_mermaid_er_column_marks_fk():
+    column = Column(name="parent_id", type="INTEGER", is_foreign_key=True)
+    result = column.to_mermaid_er()
+    assert result == " INTEGER parent_id FK"
+
+
+def test_mermaid_er_column_marks_pk_and_fk():
+    column = Column(name="association_id", type="UUID", is_key=True, is_foreign_key=True)
+    result = column.to_mermaid_er()
+    assert result == " UUID association_id PK, FK"
